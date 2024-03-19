dfdfddb90c89478d0b21f3d35f9e2d3ac1264757
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-19 14:21:38 +0530

    slogans

---
+++ Newser/src/Components/data.js
@@ -0,0 +1,29 @@
+// const slogans = ["Ink spills truth, headlines roar.","Embrace Truth, Anchor Reality Firmly.","The voice of changing region","Every Story, Every Angle","Informing, Inspiring, Innovating"]
+
+const slogans = [{
+  quote : `“Newspapers cannot be defined by the second word — paper.  They’ve got to be defined by the first word — news.”`,
+  author : `— Arthur Sulzberger, Jr.`
+},{
+  quote : `“A good newspaper is a nation talking to itself.”`,
+  author : `— Arthur Miller`
+},{
+  quote : `“People don’t actually read newspapers.  They step into them every morning like a hot bath.”`,author : `— Marshall McLuhan`
+},{
+  quote : `“I’d love to rise from the grave every ten years or so and go buy a few newspapers.”`,
+  author : `— Luis Bunuel`
+},{
+  quote : `“Newspapers are tutors as well as informers.”`,
+  author : `— Neil Kinnock`
+},{
+  quote : `“The newspaper is a greater treasure to the people than uncounted millions of gold.”`,
+  author : '— Henry Ward Beecher'
+},{quote : `“Bad news travels at the speed of light; good news travels like molasses.”`,
+author : `— Tracy Morgan`
+},{
+  quote : `“Newspapers could bring about a change in the lives of the millions of oppressed people.”`,
+  author : `— Dr. B.R. Ambedkar`
+},{
+  quote : `“Ink spills truth, headlines roar.”`,
+  author : `— ----`
+}]
+export default slogans
