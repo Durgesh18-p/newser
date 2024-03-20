313f0c7fac524b0ea36177ddaa24eb5a5eb7f716
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-20 17:46:25 +0530

    data updated

--- Newser/src/Components/data.js
+++ Newser/src/Components/data.js
@@ -1,6 +1,6 @@
 // const slogans = ["Ink spills truth, headlines roar.","Embrace Truth, Anchor Reality Firmly.","The voice of changing region","Every Story, Every Angle","Informing, Inspiring, Innovating"]
 
-const slogans = [{
+export const slogans = [{
   quote : `“Newspapers cannot be defined by the second word — paper.  They’ve got to be defined by the first word — news.”`,
   author : `— Arthur Sulzberger, Jr.`
 },{
@@ -26,4 +26,71 @@ author : `— Tracy Morgan`
   quote : `“Ink spills truth, headlines roar.”`,
   author : `— ----`
 }]
-export default slogans
+
+
+export const countries = [
+  { code: "ae", name: "United Arab Emirates" },
+  { code: "ar", name: "Argentina" },
+  { code: "at", name: "Austria" },
+  { code: "au", name: "Australia" },
+  { code: "be", name: "Belgium" },
+  { code: "bg", name: "Bulgaria" },
+  { code: "br", name: "Brazil" },
+  { code: "ca", name: "Canada" },
+  { code: "ch", name: "Switzerland" },
+  { code: "cn", name: "China" },
+  { code: "co", name: "Colombia" },
+  { code: "cu", name: "Cuba" },
+  { code: "cz", name: "Czech Republic" },
+  { code: "de", name: "Germany" },
+  { code: "eg", name: "Egypt" },
+  { code: "fr", name: "France" },
+  { code: "gb", name: "United Kingdom (Great Britain)" },
+  { code: "gr", name: "Greece" },
+  { code: "hk", name: "Hong Kong" },
+  { code: "hu", name: "Hungary" },
+  { code: "id", name: "Indonesia" },
+  { code: "ie", name: "Ireland" },
+  { code: "il", name: "Israel" },
+  { code: "in", name: "India" },
+  { code: "it", name: "Italy" },
+  { code: "jp", name: "Japan" },
+  { code: "kr", name: "South Korea" },
+  { code: "lt", name: "Lithuania" },
+  { code: "lv", name: "Latvia" },
+  { code: "ma", name: "Morocco" },
+  { code: "mx", name: "Mexico" },
+  { code: "my", name: "Malaysia" },
+  { code: "ng", name: "Nigeria" },
+  { code: "nl", name: "Netherlands" },
+  { code: "no", name: "Norway" },
+  { code: "nz", name: "New Zealand" },
+  { code: "ph", name: "Philippines" },
+  { code: "pl", name: "Poland" },
+  { code: "pt", name: "Portugal" },
+  { code: "ro", name: "Romania" },
+  { code: "rs", name: "Serbia" },
+  { code: "ru", name: "Russia" },
+  { code: "sa", name: "Saudi Arabia" },
+  { code: "se", name: "Sweden" },
+  { code: "sg", name: "Singapore" },
+  { code: "si", name: "Slovenia" },
+  { code: "sk", name: "Slovakia" },
+  { code: "th", name: "Thailand" },
+  { code: "tr", name: "Turkey" },
+  { code: "tw", name: "Taiwan" },
+  { code: "ua", name: "Ukraine" },
+  { code: "us", name: "United States" },
+  { code: "ve", name: "Venezuela" },
+  { code: "za", name: "South Africa" }
+];
+
+export const categories = [
+  { code: "business", name: "Business" },
+  { code: "entertainment", name: "Entertainment" },
+  { code: "general", name: "General" },
+  { code: "health", name: "Health" },
+  { code: "science", name: "Science" },
+  { code: "sports", name: "Sports" },
+  { code: "technology", name: "Technology" }
+];
