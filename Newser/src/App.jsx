0b32b9d5e9591fc50736f819d665246bc296f3ca
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-27 09:37:45 +0530

    routing done

--- Newser/src/App.jsx
+++ Newser/src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
-// import Header from "./Components/Header";
-// import Navigation from "./Components/Navigation";
-// import HomePage from "./Components/HomePage";
-// import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import HomePage from "./Components/HomePage";
+import Footer from "./Components/Footer";
 import Login from "./Components/Login";
+import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.news.isAuthenticated);
+
   return (
     <>
-      <Login></Login>
-      {/* <Header></Header>
-      <Navigation />
-      <HomePage />
-      <Footer /> */}
+      {isAuthenticated ? (
+        <>
+          <Header />
+          <Navigation />
+          <HomePage />
+          <Footer />
+        </>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      )}
     </>
   );
 }
