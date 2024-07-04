7f90f0fe68eb14d1c5836cec8cf73e706ef889db
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-07-04 09:59:20 +0530

    Header updated

--- Newser/src/Components/Header.jsx
+++ Newser/src/Components/Header.jsx
@@ -15,56 +15,53 @@ const Header = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(indexIncrement());
-    }, 3000);
+    }, 4000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div className="w-[100vw] h-[200px] bg-[#ecf0e7] grid place-items-center">
-      <div className="flex justify-between items-center w-[100%] pr-[100px] pl-[55px] ">
-        {" "}
+    <div className="w-full min-h-[200px] bg-[#f5f5f5] grid place-items-center py-4">
+      <div className="flex justify-between items-center w-full px-4 md:px-8 lg:px-16">
         <button
-          onClick={() => {
-            handleLogout();
-          }}
-          className=" p-[5px] border-[1px] border-[#000000] rounded font-[500]"
+          onClick={handleLogout}
+          className="p-2 border border-black rounded font-medium bg-white hover:bg-gray-200 transition duration-200"
         >
           LogOut
         </button>
       </div>
-      <h1 className="tiro-devanagari text-center fw-[700] text-[65px] ">
+      <h1 className="tiro-devanagari text-center font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
         N
-        <span className="tiro-devanagari text-center fw-[400] text-[18px] mr-[5px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mr-1">
           orth
         </span>
         E
-        <span className="tiro-devanagari text-center fw-[400] text-[18px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
           ast
         </span>
         W
-        <span className="tiro-devanagari text-center fw-[400] text-[18px] mr-[5px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mr-1">
           est
         </span>
         S
-        <span className="tiro-devanagari text-center fw-[400] text-[18px] mr-[5px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mr-1">
           outh
         </span>
         E
-        <span className="tiro-devanagari text-center fw-[400] text-[18px] mr-[5px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mr-1">
           very
         </span>
         N
-        <span className="tiro-devanagari text-center fw-[400] text-[18px] mr-[5px]">
+        <span className="tiro-devanagari text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mr-1">
           ews
         </span>
       </h1>
 
-      <div className="text-center">
-        <p className="tiro-devanagari text-center fw-[400] text-[18px]">
+      <div className="text-center mt-8 px-4">
+        <p className="tiro-devanagari text-medium sm:text-lg md:text-xl lg:text-xl xl:text-3xl">
           {slogans[index].quote}
         </p>
-        <span className="edu-tas text-center fw-[300] text-[14px]">
+        <span className="edu-tas text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
           {slogans[index].author}
         </span>
       </div>
